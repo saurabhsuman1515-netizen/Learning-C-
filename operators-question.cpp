fb17214eabb5eac9d8007fc531cd2560407801cc
saurabh suman <saurabhsuman15154@gmail.com>
2026-05-10 17:12:27 +0530

    6thcommit

---
+++ operators-question.cpp
@@ -0,0 +1,36 @@
+//area of circle
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  float r=5;
+  float a=3.1414*r*r;
+  cout<< a <<endl;
+  return 0;
+}
+
+//simple intrest find
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float p=2000, r=5, t=5;
+    float si=(p*r*t)/100;
+    cout<<"the simple intrest is:";
+    cout<<si;
+    return 0;
+}
+
+//volume of sphere
+
+#include <iostream>
+using namespace std;
+int main(){
+    float r=4.5;
+    float v=3.1414*r*r*r;
+    cout<<"volume of sphere is :";
+    cout<< v;
+    return 0;
+}
