b4d162201d69f1420dd184a6e0aea9c9d7f4397a
saurabh suman <saurabhsuman15154@gmail.com>
2026-05-10 16:35:33 +0530

    5thcommit

---
+++ conditionals.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 5;
+    // If statement
+    if (a > b) {
+        cout << "a is greater than b" << endl;
+    }
+    // If-else statement
+    if (a < b) {    
+        cout << "a is less than b" << endl;
+    } else {
+        cout << "a is not less than b" << endl;
+    }
+    return 0;
+}
