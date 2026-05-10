09cea5f284cd84f18d2cad58037ffb5cb35ee482
saurabh suman <saurabhsuman15154@gmail.com>
2026-05-10 16:29:49 +0530

    3rdcommit

---
+++ input&output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Input from the user
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    // Output the value
+    cout << "You entered: " << age << endl;
+
+    return 0;
+}
