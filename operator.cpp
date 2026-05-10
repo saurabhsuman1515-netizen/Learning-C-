7d13a5016ed9eda1d54b727e6a2743185d3b0fca
saurabh suman <saurabhsuman15154@gmail.com>
2026-05-10 16:32:48 +0530

    4thcommit

---
+++ operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 5;  
+    // Addition
+    int sum = a + b;
+    // Subtraction
+    int difference = a - b;
+    // Multiplication
+    int product = a * b;
+    // Division
+    int quotient = a / b;
+    // Modulus
+    int remainder = a % b;
+
+    // Output the results
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Product: " << product << endl;
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+
+    return 0;
+}
