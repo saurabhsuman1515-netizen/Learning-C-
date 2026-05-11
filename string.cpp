1b3db9316f9e2ac861bb2c63c3754a3de44e3734
saurabh suman <saurabhsuman15154@gmail.com>
2026-05-12 00:25:27 +0530

    7thcommit

---
+++ string.cpp
@@ -0,0 +1,48 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char s[7]={'s','a','u','r','a','b','h'};
+//     for (int i=0;i<7;i++){
+//         cout<<s[i]<<" ";
+//     }
+
+//     #include <iostream>
+// using namespace std;
+// int main(){
+//     string name="saurabh is a student";
+//     cout<<name;
+//     return 0;
+// }
+
+//     #include <iostream>
+// using namespace std;
+// int main(){
+//     string name;
+//     cout<<"enter the string:";
+//     cin>>name;
+//     cout<<name;
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main()
+// std::string str={"welcome"};
+// cout<< str;
+// return 0;
+
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main(){
+//     string f_name,l_name;
+//     cout<<"enter your first name : ";
+//     getline(cin,f_name);
+//     cout<<"enter your last name : ";
+//     getline(cin,l_name);
+//     cout<<"my first name is : "<<f_name<<f_name.length()<<" characters, ";
+//     cout<<"my last name is : "<<l_name<<l_name.length()<<"characters";
+//     cout<<f_name.append(l_name);
+// }
